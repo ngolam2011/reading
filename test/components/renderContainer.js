7265a4750232317398cdc24ac79e6f50082a89a1
ngo lam <ngolam>
2018-05-21 21:57:40 +0700

    a

--- test/components/renderContainer.js
+++ test/components/renderContainer.js
@@ -1,7 +1,14 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 export default (component, store) => {
   const context = {
     store,
   }
   return shallow(component, { context, disableLifecycleMethods: true })
+}
+
+export const mountWithStore =  (component, store) => {
+  const context = {
+    store,
+  }
+  return mount(component, { context, disableLifecycleMethods: true })
 }
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,29 @@
 // setup file
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-configure({ adapter: new Adapter() });
+function setUpDomEnvironment() {
+    const dom = new JSDOM('<!doctype html><html><body></body></html>', {url: 'http://localhost/'})
+    const { window } = dom
+
+    global.window = window
+    global.document = window.document
+    global.navigator = {
+        userAgent: 'node.js',
+    }
+    copyProps(window, global)
+}
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop))
+  Object.defineProperties(target, props)
+}
+
+setUpDomEnvironment()
+
+
+
+configure({ adapter: new Adapter() })
